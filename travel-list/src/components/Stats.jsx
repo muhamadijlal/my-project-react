42be0abccc9048ffa005bbae86c9bae253f39266
muhamadijlal <haidarijlal027@gmail.com>
2023-09-23 20:22:45 +0700

    add: added coditional rendering in stats component

--- travel-list/src/components/Stats.jsx
+++ travel-list/src/components/Stats.jsx
@@ -1,14 +1,26 @@
 const Stats = ({ items }) => {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+
   const totalItem = items.length;
   const totalPackedItem = items.filter((item) => item.packed).length;
   const percentage = (totalPackedItem / totalItem) * 100;
 
   return (
     <footer className="stats">
-      <em>
-        💼 You have {totalItem} items on your list, and you already packed
-        {totalPackedItem} ({Math.round(percentage) || 0}%)
-      </em>
+      {percentage === 100 ? (
+        "You got everything! Ready to go ✈️"
+      ) : (
+        <em>
+          💼 You have {totalItem} items on your list, and you already packed
+          {totalPackedItem} ({Math.round(percentage) || 0}%)
+        </em>
+      )}
     </footer>
   );
 };
