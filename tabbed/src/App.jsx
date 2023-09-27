d1c84b78f1418039617327c07fe983de6408c3ce
muhamadijlal <haidarijlal027@gmail.com>
2023-09-27 17:24:11 +0700

    init commit

---
+++ tabbed/src/App.jsx
@@ -0,0 +1,12 @@
+import Tabbed from "./components/Tabbed";
+import content from "./data";
+
+function App() {
+  return (
+    <div>
+      <Tabbed content={content} />
+    </div>
+  );
+}
+
+export default App;
