6b6e564b3094ed10976c1ae131812f62f8068b06
muhamadijlal <haidarijlal027@gmail.com>
2023-10-05 10:04:08 +0700

    init commit

---
+++ react-quiz/README.md
@@ -0,0 +1,8 @@
+# react quiz app
+
+first to do after clone this project :
+
+```
+npm install
+npm run dev
+```
---
+++ react-quiz/src/App.jsx
@@ -0,0 +1,7 @@
+import DateCounter from "./components/DateCounter";
+
+function App() {
+  return <DateCounter />;
+}
+
+export default App;
